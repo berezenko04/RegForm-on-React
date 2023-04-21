43ccc90453686de7cb8ca79ac1caacb3b2bf9143
berezenko04 <koffld@yahoo.com>
2023-04-21 03:08:13 +0300

    fix: fixed base in package.json and vite.config for deploy in git pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/RegForm-on-React' element={<Home />} />
       </Routes>
     </div>
   );
